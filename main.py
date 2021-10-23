4a6bfa5e3e475469e40ac8d1ddb010475edfe919
Fadjar Pradja Winata <49338745+fadjarpw@users.noreply.github.com>
2021-10-23 17:13:12 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+for game in range(1, 100):
+
+  if game % 3 == 0 and game % 5 == 0:
+    print("FizzBuzz")
+  elif game % 3 == 0:
+    print("Fizz")
+  elif game % 5 == 0:
+    print("Buzz")
+  else:
+    print(game)
+
+
+print("End Of Lifeeeeee")
+  
